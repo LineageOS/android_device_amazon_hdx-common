63848bc0346c425a6ea52941839f54a87d687f81
Hashcode <hashcode0f@gmail.com>
2014-06-30 21:44:55 -0700

    common device setup update

--- hdx-common.mk
+++ hdx-common.mk
@@ -1,18 +1,45 @@
+COMMON_FOLDER := device/amazon/hdx-common
+
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-## Get non-open-source specific aspects
+# Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/amazon/hdx-common/hdx-common-vendor.mk)
 
-## AOSP overlay
-DEVICE_PACKAGE_OVERLAYS += device/amazon/hdx-common/overlay/aosp
+# AOSP overlay
+DEVICE_PACKAGE_OVERLAYS += $(COMMON_FOLDER)/overlay/aosp
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal hdpi xhdpi xxhdpi
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/native/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml \
+    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
+    frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
+    packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
+
 # Audio configuration
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml
+
+# Extended media support
+PRODUCT_PACKAGES += \
+    qcmediaplayer
+
+PRODUCT_BOOT_JARS += qcmediaplayer
 
 # Ramdisk
 PRODUCT_PACKAGES += \
@@ -22,30 +49,145 @@ PRODUCT_PACKAGES += \
     init.target.rc \
     ueventd.qcom.rc
 
-# NFC packages
+# Audio
 PRODUCT_PACKAGES += \
-    libnfc-nci \
-    libnfc_nci_jni \
-    nfc_nci.msm8974 \
-    NfcNci \
-    Tag \
-    com.android.nfc_extras
+    audiod \
+    libqcomvisualizer \
+    libqcomvoiceprocessing \
+    tinymix
+
+# Wifi
+PRODUCT_PACKAGES += \
+    libnetcmdiface \
+    macloader \
+    crda \
+    regulatory.bin \
+    linville.key.pub.pem
 
 # Set default USB interface
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     persist.sys.usb.config=mtp
 
-# Lights
-PRODUCT_PACKAGES += lights.msm8974
+# QC Perf
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.extension_library=/vendor/lib/libqc-opt.so
+
+# Common build.props
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.chipname=msm8974 \
+    ro.sf.lcd_density=320 \
+    ro.opengles.version=196608 \
+    persist.timed.enable=true \
+    keyguard.no_require_sim=true
+
+# Radio props
+PRODUCT_PROPERTY_OVERRIDES += \
+    rild.libpath=/system/lib/libril-lab126qmi.so \
+    ril.subscription.types=NV,RUIM \
+    ro.use_data_netmgrd=true \
+    persist.data.netmgrd.qos.enable=true \
+    persist.rild.nitz_plmn= \
+    persist.rild.nitz_long_ons_0= \
+    persist.rild.nitz_long_ons_1= \
+    persist.rild.nitz_long_ons_2= \
+    persist.rild.nitz_long_ons_3= \
+    persist.rild.nitz_short_ons_0= \
+    persist.rild.nitz_short_ons_1= \
+    persist.rild.nitz_short_ons_2= \
+    persist.rild.nitz_short_ons_3= \
+    DEVICE_PROVISIONED=1 \
+    ro.telephony.default_network=9 \
+    ro.ril.usb.port.serial=ttyUSB \
+    ro.ril.usb.port.qmi=qmi_usb \
+    ro.ril.usb.port.rmnet=rmnet_usb
+
+# Audio
+PRODUCT_PACKAGES += \
+    audio_policy.msm8974 \
+    audio.primary.msm8974 \
+    audio.a2dp.default \
+    audio.usb.default \
+    audio.r_submix.default \
+    libaudio-resampler
+
+# HALs
+PRODUCT_PACKAGES += \
+    lights.msm8974 \
+    copybit.msm8974 \
+    gralloc.msm8974 \
+    hwcomposer.msm8974 \
+    power.msm8974 \
+    memtrack.msm8974 \
+    camera.msm8974
+
+# Misc
+PRODUCT_PACKAGES += \
+    com.android.future.usb.accessory
+
+# Live Wallpapers
+PRODUCT_PACKAGES += \
+    LiveWallpapers \
+    LiveWallpapersPicker \
+    VisualizationWallpapers \
+    librs_jni
 
 # Media config
-PRODUCT_PACKAGES += media_codecs.xml
+PRODUCT_COPY_FILES += \
+    $(COMMON_FOLDER)/prebuilt/etc/media_codecs.xml:system/etc/media_codecs.xml
+
+# QCOM Display
+PRODUCT_PACKAGES += \
+    libgenlock \
+    libmemalloc \
+    liboverlay \
+    libqdutils
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.sf.hw=1 \
+    debug.egl.hw=1 \
+    persist.hwc.mdpcomp.enable=true \
+    debug.mdpcomp.logs=0
+
+# Omx
+PRODUCT_PACKAGES += \
+    libdivxdrmdecrypt \
+    libmm-omxcore \
+    libOmxCore \
+    libc2dcolorconvert \
+    libOmxVdecHevc \
+    libstagefrighthw \
+    libOmxVdec \
+    libOmxVenc \
+    libOmxAacEnc \
+    libOmxAmrEnc \
+    libOmxEvrcEnc \
+    libOmxQcelp13Enc \
+    libdashplayer
+
+# Filesystem management tools
+PRODUCT_PACKAGES += \
+    make_ext4fs \
+    e2fsck \
+    setup_fs
 
 # QRNGD
-PRODUCT_PACKAGES += qrngd
+PRODUCT_PACKAGES += \
+    qrngd \
+    qrngp
 
 # Recovery
-PRODUCT_COPY_FILES += device/amazon/hdx-common/twrp.fstab:recovery/root/etc/twrp.fstab
+PRODUCT_COPY_FILES += $(COMMON_FOLDER)/twrp.fstab:recovery/root/etc/twrp.fstab
+
+# Keymaster
+PRODUCT_PACKAGES += keystore.msm8974
+
+# Charger
+PRODUCT_PACKAGES += charger charger_res_images
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+
+# We have enough storage space to hold precise GC data
+PRODUCT_TAGS += dalvik.gc.type-precise
 
 # call dalvik heap config
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)

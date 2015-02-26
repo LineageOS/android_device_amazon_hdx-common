b5d65438602ed6ece1916dfa6334c03dca29dfd6
cpasjuste <cpasjuste@gmail.com>
2015-02-26 10:05:25 +0100

    simplify/clean mkboot.mk

--- mkboot.mk
+++ mkboot.mk
@@ -1,34 +1,17 @@
 # use manually appened kernel+dtb
 ifeq ($(TARGET_KERNEL_CONFIG),apollo-cyanogenmod_defconfig)
-DTS_NAMES := apollo
+DTS_NAME := apollo-v2
 endif
 ifeq ($(TARGET_KERNEL_CONFIG),thor-cyanogenmod_defconfig)
-DTS_NAMES := thor
+DTS_NAME := thor-v2
 endif
-
-DTS_FILES = $(wildcard $(TOP)/$(TARGET_KERNEL_SOURCE)/arch/arm/boot/dts/$(DTS_NAME)*.dts)
-DTS_FILE = $(lastword $(subst /, ,$(1)))
-DTB_FILE = $(addprefix $(KERNEL_OUT)/arch/arm/boot/,$(patsubst %.dts,%.dtb,$(call DTS_FILE,$(1))))
-ZIMG_FILE = $(addprefix $(KERNEL_OUT)/arch/arm/boot/,$(patsubst %.dts,%-zImage,$(call DTS_FILE,$(1))))
-KERNEL_ZIMG = $(KERNEL_OUT)/arch/arm/boot/zImage
-DTC = $(KERNEL_OUT)/scripts/dtc/dtc
-
-define append-dtb
-mkdir -p $(KERNEL_OUT)/arch/arm/boot;\
-rm -f $(KERNEL_OUT)/arch/arm/boot/*.dtb;\
-rm -f $(KERNEL_OUT)/arch/arm/boot/*.dtb_dts;\
-$(foreach DTS_NAME, $(DTS_NAMES), \
-   $(foreach d, $(DTS_FILES), \
-      $(DTC) -p 2048 -O dtb -o $(call DTB_FILE,$(d)) $(d); \
-      $(DTC) -p 2048 -O dts -o $(call DTB_FILE,$(d))_dts $(d); \
-      cat $(KERNEL_ZIMG) $(call DTB_FILE,$(d)) > $(call ZIMG_FILE,$(d));))
-endef
 
 # Hacked boot image
 $(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES)
 	@echo -e ${CL_CYN}"Building hdx hacked boot image: $@"${CL_RST}
-	$(append-dtb)
-	cp $(KERNEL_OUT)/arch/arm/boot/$(DTS_NAMES)-v2-zImage $(PRODUCT_OUT)/kernel
+	$(KERNEL_OUT)/scripts/dtc/dtc -p 2048 -O dtb -o $(KERNEL_OUT)/arch/arm/boot/$(DTS_NAME).dtb \
+		$(TARGET_KERNEL_SOURCE)/arch/arm/boot/dts/$(DTS_NAME).dts
+	cat $(KERNEL_OUT)/arch/arm/boot/zImage $(KERNEL_OUT)/arch/arm/boot/$(DTS_NAME).dtb > $(PRODUCT_OUT)/kernel
 	$(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@
 	cd ./device/amazon/hdx-common/Cuber/ && \
 	make && \
@@ -41,8 +24,9 @@ $(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES)
 # Hacked recovery image
 $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) $(recovery_ramdisk) $(recovery_kernel)
 	@echo -e ${CL_CYN}"Building hdx hacked recovery image: $@"${CL_RST}
-	$(append-dtb)
-	cp $(KERNEL_OUT)/arch/arm/boot/$(DTS_NAMES)-v2-zImage $(PRODUCT_OUT)/kernel
+	$(KERNEL_OUT)/scripts/dtc/dtc -p 2048 -O dtb -o $(KERNEL_OUT)/arch/arm/boot/$(DTS_NAME).dtb \
+		$(TARGET_KERNEL_SOURCE)/arch/arm/boot/dts/$(DTS_NAME).dts
+	cat $(KERNEL_OUT)/arch/arm/boot/zImage $(KERNEL_OUT)/arch/arm/boot/$(DTS_NAME).dtb > $(PRODUCT_OUT)/kernel
 	$(hide) $(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@
 	cd ./device/amazon/hdx-common/Cuber/ && \
 	make && \

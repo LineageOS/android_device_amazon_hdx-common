14bad3da0d883a676807c9bb4aa4bbe42d56c7d9
Eddi De Pieri <eddi@depieri.net>
2015-03-28 15:47:59 +0100

    make pyCuber.sh to work outside current dir

--- pyCuber/pyCuber.sh
+++ pyCuber/pyCuber.sh
@@ -24,36 +24,17 @@ vrfy() {
 openssl genrsa -3 2048 > $k
 
 echo
-echo "Example #1: signing 0x1234567890"
+echo "signing '$1'"
 echo
 
-echo 0x1234567890 | openssl rsautl -sign -inkey $k > $s
-vrfy
-
-echo
-echo "Decrypted FORGED signature (showing padding)"
-echo 0x1234567890 | python cuber.py | openssl rsautl -verify -inkey $k -hexdump -raw
-echo
-
-
-echo
-echo "Example #2: signing '$1'"
-echo
-
-off=$(python cuboot.py "$1")
+off=$(python ${0%/*}/cuboot.py "$1")
 echo "$off"
 off=${off##*offset=}
 off=${off%%$'\n'*}
 
-python split.py "$1" $off $i
-python split.py $i.1 256 $s
+python ${0%/*}/split.py "$1" $off $i
+python ${0%/*}/split.py $i.1 256 $s
 
 openssl dgst -sha256 -binary < $i.0 | openssl rsautl -sign -inkey $k > $s
-vrfy
-
-echo
-echo "Decrypted FORGED signature (showing padding)"
-openssl rsautl -verify -inkey $k -hexdump -raw < $s.0
-echo
 
 rm -fr $d

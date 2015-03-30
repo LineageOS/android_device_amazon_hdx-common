3b28f56dc83bed44963aa361863352232986f0dd
Eddi De Pieri <eddi@depieri.net>
2015-03-30 13:59:16 -0700

    change mkboot.mk to use python cuber implementation by Draxie
    [hashcode0f@gmail.com: used COMMON_FOLDER define for location]
    Change-Id: If951763e25e25463e9fa8ecfd13ca2acddd16ba3

--- mkboot.mk
+++ mkboot.mk
@@ -14,11 +14,7 @@ $(INSTALLED_DTIMAGE_TARGET): $(DTBTOOL) $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/u
 $(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES) $(INSTALLED_DTIMAGE_TARGET)
 	@echo -e ${CL_CYN}"Building hdx hacked boot image: $@"${CL_RST}
 	$(hide) $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --dt $(INSTALLED_DTIMAGE_TARGET) --output $@
-	cd ./device/amazon/hdx-common/Cuber/ && \
-	make Cuber-boot && \
-	./Cuber-boot --sign $@ $@.signed && \
-	cd -
-	$(hide) mv $@.signed $@
+	$(hide) $(COMMON_FOLDER)/pyCuber/pyCuber.sh $@
 	$(hide) $(call assert-max-image-size,$@,$(BOARD_BOOTIMAGE_PARTITION_SIZE),raw)
 	@echo -e ${CL_CYN}"Made hdx hacked boot image: $@"${CL_RST}
 
@@ -28,10 +24,6 @@ $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) $(INSTALLED_DTIMAGE_TARGET) \
 		$(recovery_kernel)
 	@echo -e ${CL_CYN}"Building hdx hacked recovery image: $@"${CL_RST}
 	$(hide) $(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --dt $(INSTALLED_DTIMAGE_TARGET) --output $@
-	cd ./device/amazon/hdx-common/Cuber/ && \
-	make Cuber-recovery && \
-	./Cuber-recovery --sign $@ $@.signed && \
-	cd -
-	$(hide) mv $@.signed $@
+	$(hide) $(COMMON_FOLDER)/pyCuber/pyCuber.sh $@
 	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE),raw)
 	@echo -e ${CL_CYN}"Made hdx hacked recovery image: $@"${CL_RST}

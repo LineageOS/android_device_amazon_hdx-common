c658790fe426bfcccff2ab7b491ea8dd81fd8afd
Hashcode <hashcode0f@gmail.com>
2015-04-01 10:58:37 -0700

    remove CM updater URL override
    Change-Id: I046b9893cdb36e741e7b8c6a55104c63cba66d62

--- hdx-common.mk
+++ hdx-common.mk
@@ -274,10 +274,6 @@ ADDITIONAL_BUILD_PROPERTIES += \
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
-# CM updater
-PRODUCT_PROPERTY_OVERRIDES += \
-	cm.updater.uri="http://kfirehdx.ddns.net/CyanogenModOTA/api"
-
 # We have enough storage space to hold precise GC data
 PRODUCT_TAGS += dalvik.gc.type-precise
 

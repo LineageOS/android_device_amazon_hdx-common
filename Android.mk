c40468a33782058e9f7f0efee6500e27386b2c33
Hashcode <hashcode0f@gmail.com>
2015-06-04 13:59:24 -0700

    hdx-common: add firmware symlinks to build
    This allows us to fix selinux denials and remove:
    init.qcom.syspart_fixup.sh + init.qcom.modem_links.sh
    
    Change-Id: I708cc494221a19a1119846272a223e3832580e9d

--- Android.mk
+++ Android.mk
@@ -4,6 +4,19 @@ ifneq ($(filter apollo thor,$(TARGET_DEVICE)),)
 
 ifneq ($(TARGET_SIMULATOR),true)
 include $(call first-makefiles-under,$(LOCAL_PATH))
+
+include $(CLEAR_VARS)
+
+FIRMWARE_DXHDCP2_IMAGES := \
+    dxhdcp2.b00 dxhdcp2.b01 dxhdcp2.b02 dxhdcp2.b03 dxhdcp2.mdt
+FIRMWARE_DXHDCP2_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(FIRMWARE_DXHDCP2_IMAGES)))
+$(FIRMWARE_DXHDCP2_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "DXHDCP2 Firmware link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /firmware/image/$(notdir $@) $@
+ALL_DEFAULT_INSTALLED_MODULES += $(FIRMWARE_DXHDCP2_SYMLINKS)
+
 endif
 
 endif

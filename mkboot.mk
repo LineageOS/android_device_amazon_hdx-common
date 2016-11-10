b0512d2af937cff152979f78c6bc0f1a4d52e15d
Hashcode <hashcode0f@gmail.com>
2016-11-09 22:48:37 -0800

    use LZMA compression for recovery ramdisk
    recovery partition on Kindle HDX devices is only 10mb.  We need to
    make sure we squeeze everything as small as possible.
    
    Change-Id: Ie0350db83187234656394e883fa1f9fff57032d7

--- mkboot.mk
+++ mkboot.mk
@@ -3,6 +3,7 @@ LOCAL_PATH := $(call my-dir)
 ## Build and run dtbtool
 DTBTOOL := $(HOST_OUT_EXECUTABLES)/dtbToolCM$(HOST_EXECUTABLE_SUFFIX)
 INSTALLED_DTIMAGE_TARGET := $(PRODUCT_OUT)/dt.img
+LZMA := $(shell which lzma)
 
 $(INSTALLED_DTIMAGE_TARGET): $(DTBTOOL) $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr $(INSTALLED_KERNEL_TARGET)
 	@echo -e ${CL_CYN}"Start DT image: $@"${CL_RST}
@@ -20,10 +21,14 @@ $(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES) $(INSTAL
 
 # Hacked recovery image
 $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) $(INSTALLED_DTIMAGE_TARGET) \
-		$(recovery_ramdisk) \
+		$(recovery_uncompressed_ramdisk) \
 		$(recovery_kernel)
 	@echo -e ${CL_CYN}"Building hdx hacked recovery image: $@"${CL_RST}
-	$(hide) $(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --dt $(INSTALLED_DTIMAGE_TARGET) --output $@
+	$(hide) rm -f $(OUT)/ramdisk-recovery.cpio.lzma
+	@echo -e ${CL_CYN}"Compressing recovery ramdisk..."${CL_RST}
+	$(hide) $(LZMA) -z -9 $(recovery_uncompressed_ramdisk)
+	$(hide) cp $(recovery_uncompressed_ramdisk).lzma $(recovery_ramdisk)
+	$(hide) $(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) $(INTERNAL_MKBOOTIMG_VERSION_ARGS) $(BOARD_MKBOOTIMG_ARGS) --dt $(INSTALLED_DTIMAGE_TARGET) --output $@
 	$(hide) $(COMMON_FOLDER)/pyCuber/pyCuber.sh $@
-	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE),raw)
+	#$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE),raw)
 	@echo -e ${CL_CYN}"Made hdx hacked recovery image: $@"${CL_RST}

878e1b0fa344f5d97166f73b93b654587b2a1578
Hashcode <hashcode0f@gmail.com>
2016-12-12 13:55:35 -0800

    add gps/location config files to configs/etc
    these have been removed from vendor so they can be updated over time
    
    Change-Id: Ife3639a21ba992da656f1c1c3b5b48777cec2f42

--- hdx-common.mk
+++ hdx-common.mk
@@ -99,6 +99,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
 	gps.msm8974
 
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/configs/etc/flp.conf:system/etc/flp.conf \
+	$(LOCAL_PATH)/configs/etc/gps.conf:system/etc/gps.conf \
+	$(LOCAL_PATH)/configs/etc/izat.conf:system/etc/izat.conf \
+	$(LOCAL_PATH)/configs/etc/lowi.conf:system/etc/lowi.conf \
+
 # Lights
 PRODUCT_PACKAGES += \
 	lights.msm8974

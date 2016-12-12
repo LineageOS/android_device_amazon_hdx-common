b287bc94de53f77322d00920531bc1825ccafaf3
Hashcode <hashcode0f@gmail.com>
2016-12-12 13:54:44 -0800

    refactor configs into configs/etc
    also drop duplicate audio_effects.conf file which is generated
    in the build
    
    Change-Id: I8a390684608840391c99fd8f3f2b4b241c08c7f1

--- hdx-common.mk
+++ hdx-common.mk
@@ -50,14 +50,12 @@ PRODUCT_PACKAGES += \
 
 # Audio/Media
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/configs/vendor/audio_effects.conf:system/vendor/etc/audio_effects.conf \
-	$(LOCAL_PATH)/configs/audio_effects.conf:system/etc/audio_effects.conf \
-	$(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf \
-	$(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
-	$(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
-	$(LOCAL_PATH)/configs/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
-	$(LOCAL_PATH)/configs/mixer_paths.xml:system/etc/mixer_paths.xml \
-	$(LOCAL_PATH)/configs/mixer_paths.xml:system/etc/mixer_paths_auxpcm.xml \
+	$(LOCAL_PATH)/configs/etc/audio_policy.conf:system/etc/audio_policy.conf \
+	$(LOCAL_PATH)/configs/etc/media_profiles.xml:system/etc/media_profiles.xml \
+	$(LOCAL_PATH)/configs/etc/media_codecs.xml:system/etc/media_codecs.xml \
+	$(LOCAL_PATH)/configs/etc/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
+	$(LOCAL_PATH)/configs/etc/mixer_paths.xml:system/etc/mixer_paths.xml \
+	$(LOCAL_PATH)/configs/etc/mixer_paths.xml:system/etc/mixer_paths_auxpcm.xml \
 	frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
 	frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
 

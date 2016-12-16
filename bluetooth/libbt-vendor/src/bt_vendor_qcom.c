34b7b761b67ee5d09c323455c19ea43541c1abd6
Hashcode <hashcode0f@gmail.com>
2016-12-15 21:04:49 -0800

    bluetooth: libbt-vendor: add BT_NV_SUPPORT check to fix build
    Change-Id: I35597e4ea710934d44bf6183f829952eb9d7e897

--- bluetooth/libbt-vendor/src/bt_vendor_qcom.c
+++ bluetooth/libbt-vendor/src/bt_vendor_qcom.c
@@ -835,12 +835,14 @@ static int op(bt_vendor_opcode_t opcode, void *param)
                                         ignore_boot_prop = TRUE;
                                     }
 #endif //READ_BT_ADDR_FROM_PROP
+#ifdef BT_NV_SUPPORT
                                     /* Always read BD address from NV file */
                                     if(ignore_boot_prop && !bt_vendor_nv_read(1, vnd_local_bd_addr))
                                     {
                                        /* Since the BD address is configured in boot time We should not be here */
                                        ALOGI("Failed to read BD address. Use the one from bluedroid stack/ftm");
                                     }
+#endif
                                     if(rome_soc_init(fd,vnd_local_bd_addr)<0) {
                                         retval = -1;
                                         userial_clock_operation(fd, USERIAL_OP_CLK_OFF);
